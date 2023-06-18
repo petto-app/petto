a8c6a95b8d8b3b767653e0f482b04e5faadbc70a
LordRonz <aarontanhar2000@gmail.com>
2023-06-18 22:28:12 +0700

    Initial Commit

---
+++ Petto/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Petto
+//
+//  Created by Aaron Christopher Tanhar on 18/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Petto/PettoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PettoApp.swift
+//  Petto
+//
+//  Created by Aaron Christopher Tanhar on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PettoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
